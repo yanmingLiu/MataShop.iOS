ecb6f90e1a10f6a319de907cb6df2f1bb2670fe9
jobs <jobs@gmail.com>
2023-09-28 11:36:15 +0700

    no message

---
+++ MataShop/🇨🇳ServiceLogic/我的/ViewController/MSMyInfoVC/MSMyInfoVC.h
@@ -0,0 +1,16 @@
+//
+//  MSMyInfoVC.h
+//  MataShop
+//
+//  Created by Jobs Hi on 9/27/23.
+//
+
+#import "BaseViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MSMyInfoVC : BaseViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MataShop/🇨🇳ServiceLogic/我的/ViewController/MSMyInfoVC/MSMyInfoVC.m
@@ -0,0 +1,31 @@
+//
+//  MSMyInfoVC.m
+//  MataShop
+//
+//  Created by Jobs Hi on 9/27/23.
+//
+
+#import "MSMyInfoVC.h"
+
+@interface MSMyInfoVC ()
+
+@end
+
+@implementation MSMyInfoVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
